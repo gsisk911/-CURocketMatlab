b3ea52eadf4b3f8bf5fa54767aacfaf27258f34d
gsisk911 <54130672+gsisk911@users.noreply.github.com>
2021-10-08 11:31:09 -0400

    Update plot_FD_cmp.m

--- Plotting/plot_FD_cmp.m
+++ Plotting/plot_FD_cmp.m
@@ -2,7 +2,8 @@ function plot_FD_cmp(FD_wo,FD_AA,t_wo,t_AA,metric)
 
 lb2kg = 0.45359237; %pound to kilogram
 if(~metric)
-    FD = FD/(9.81*lb2kg); %Drag in Pounds
+    FD_wo = FD_wo/(9.81*lb2kg); %Drag in Pounds
+    FD_AA = FD_AA/(9.81*lb2kg); %Drag in Pounds
     str = "Drag force in lb";
 else
     str = "Drag force in newton";
@@ -14,6 +15,6 @@ hold on
 plot(t_AA,FD_AA)
 grid on
 xlabel("time t in seconds")
-ylabel("Drag force in newton")
+ylabel(str)
 legend("No Active Aero", "With Active Aero");
 end
